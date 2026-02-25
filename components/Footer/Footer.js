6131629846c6187a49d7c8992df2b735c8e1a1a3
Brian Chenoweth <brian.michael.chenoweth@gmail.com>
2026-02-25 12:33:41 -0500

    Footerupdates

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -2,9 +2,8 @@
 import classNames from 'classnames/bind';
 import {
   FaFacebookF,
-  FaInstagram,
-  FaTwitter,
-} from 'react-icons/fa';
+  FaXTwitter,
+} from 'react-icons/fa6';
 import Image from 'next/image';
 import Link from 'next/link';
 import appConfig from 'app.config.js';
@@ -20,8 +19,6 @@ export default function Footer({
   menuItems,
   navOneMenuItems,
   navTwoMenuItems,
-  resourcesMenuItems,
-  aboutMenuItems,
 }) {
   return (
     <>
@@ -37,16 +34,46 @@ export default function Footer({
 
               <div className={cx('resources')}>
                 <h3>Connect with Us</h3>
-                <NavigationMenu className={cx('quick')} menuItems={resourcesMenuItems} />
+                {appConfig?.socialLinks && (
+                  <div className={cx('social-links')}>
+                    <ul aria-label="Social media">
+
+                      {appConfig.socialLinks?.facebookUrl && (
+                        <li>
+                          <a
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className={cx('social-icon-link')}
+                            href={appConfig.socialLinks.facebookUrl}
+                          >
+                            <FaFacebookF title="Facebook" className={cx('social-icon')} />
+                          </a>
+                        </li>
+                      )}
+
+                      {appConfig.socialLinks?.twitterUrl && (
+                        <li>
+                          <a
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className={cx('social-icon-link')}
+                            href={appConfig.socialLinks.twitterUrl}
+                          >
+                            <FaXTwitter title="Twitter" className={cx('social-icon')} />
+                          </a>
+                        </li>
+                      )}
+                    </ul>
+                  </div>
+                )}
+
+                <a href="https://maps.calpoly.edu/place/bldg-052-0/@35.3006123,-120.6608836,16.6z" target="_blank" rel="noopener noreferrer" className={cx('address')}>Office: Building 52, Room D-27</a>
+                
               </div>
+
               
-              {/* <div className={cx('footer-nav')}>
-                <h3>Integration</h3>
-                <NavigationMenu className={cx('quick')} menuItems={menuItems} />
-              </div> */}
 
               <div className={cx('contact-info')}>
-                {/* modern Link */}
                 <Link href="/" className={cx('cppText')}>
                   {title ?? 'Graduate Education'}
                 </Link>
@@ -63,7 +90,6 @@ export default function Footer({
 
             <div className={cx('logo-address')}>
               <div className={cx('logo')}>
-                {/* modern Link wrapping Image */}
                 <Link href="https://www.calpoly.edu/" title="Home" target='_blank' rel='noopener noreferrer'>
                   <Image
                     src="/logo.png"
@@ -76,52 +102,7 @@ export default function Footer({
               </div>
 
               <p>1 Grand Avenue, San Luis Obispo, CA 93407</p>
-              <a href="tel:8057561111" className={cx('phone')}>(805) 756-1111</a>
 
-              {appConfig?.socialLinks && (
-                <div className={cx('social-links')}>
-                  <ul aria-label="Social media">
-                    {appConfig.socialLinks?.instagramUrl && (
-                      <li>
-                        <a
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          className={cx('social-icon-link')}
-                          href={appConfig.socialLinks.instagramUrl}
-                        >
-                          <FaInstagram title="Instagram" className={cx('social-icon')} />
-                        </a>
-                      </li>
-                    )}
-
-                    {appConfig.socialLinks?.twitterUrl && (
-                      <li>
-                        <a
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          className={cx('social-icon-link')}
-                          href={appConfig.socialLinks.twitterUrl}
-                        >
-                          <FaTwitter title="Twitter" className={cx('social-icon')} />
-                        </a>
-                      </li>
-                    )}
-
-                    {appConfig.socialLinks?.facebookUrl && (
-                      <li>
-                        <a
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          className={cx('social-icon-link')}
-                          href={appConfig.socialLinks.facebookUrl}
-                        >
-                          <FaFacebookF title="Facebook" className={cx('social-icon')} />
-                        </a>
-                      </li>
-                    )}
-                  </ul>
-                </div>
-              )}
             </div>
 
             <div className={cx('nav-one')}>
