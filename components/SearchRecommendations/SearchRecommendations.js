ce5c469f45cc8d39d58397d1c4f4ac8c0d410c2a
Brian Chenoweth <brian.michael.chenoweth@gmail.com>
2026-03-09 09:06:44 -0400

    1:8  Error: 'Link' is defined but never used.  no-unused-vars 13:49  Error: 'programTypes' is defined but never used.  no-unused-vars

--- components/SearchRecommendations/SearchRecommendations.js
+++ components/SearchRecommendations/SearchRecommendations.js
@@ -1,16 +1,13 @@
-import Link from 'next/link';
-
 import styles from './SearchRecommendations.module.scss';
 
 /**
  * Render the SearchRecommendations component.
  *
  * @param {Props} props The props object.
- * @param {Array} props.programTypes Program type links to show
  *
  * @returns {React.ReactElement} The SearchRecommendations component.
  */
-export default function SearchRecommendations({ programTypes }) {
+export default function SearchRecommendations({ }) {
   return (
     <div className={styles.recommendations}>
     </div>
